a9b429cc85635b688361f7a028182aec42cfabe3
maleicacid <4982384+kazuki0824@users.noreply.github.com>
2026-03-08 21:40:07 +0900

    Update get_android_qcow2.sh

--- get_android_qcow2.sh
+++ get_android_qcow2.sh
@@ -5,10 +5,12 @@ set -euo pipefail
 cd $(dirname "$0")
 SCRIPTDIR=$(dirname "$(realpath "${BASH_SOURCE:-0}")")
 
+VARIANT=${1:-r86s}
+
 mkdir ./build-work || true
 pushd ./build-work
 repo init -u https://github.com/kazuki0824/android --git-lfs -b lineage-21.0
 repo sync -j$(nproc) -c -d
-$SCRIPTDIR/docker/build_in_docker.sh lineage_r86s_tv_virtio
-sudo $SCRIPTDIR/image/make_selfcontained_qcow2.sh lineage_r86s_tv_virtio
+$SCRIPTDIR/docker/build_in_docker.sh lineage_${VARIANT}_tv_virtio
+sudo $SCRIPTDIR/image/make_selfcontained_qcow2.sh lineage_${VARIANT}_tv_virtio
 popd
