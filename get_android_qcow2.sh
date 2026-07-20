f6d1f7f4ab756d9e58d3b55abcad2d28132b56a2
maleicacid <4982384+kazuki0824@users.noreply.github.com>
2026-07-21 01:04:18 +0900

    Make raw image verifier executable

--- get_android_qcow2.sh
+++ get_android_qcow2.sh
@@ -191,7 +191,7 @@ sync_px4_ack_checkout
 build_px4_vendor_modules_zip
 
 "${SCRIPTDIR}/docker/build_in_docker.sh" "${PRODUCT}"
-bash "${SCRIPTDIR}/image/verify_px4_in_raw.sh" "${PRODUCT}"
+"${SCRIPTDIR}/image/verify_px4_in_raw.sh" "${PRODUCT}"
 sudo rm -f "out/target/product/${PRODUCT}/disk-vda.qcow2" || true
 sudo rm -f "out/target/product/${PRODUCT}/userdata-empty.qcow2" || true
 "${SCRIPTDIR}/image/make_disk_image.sh" \
