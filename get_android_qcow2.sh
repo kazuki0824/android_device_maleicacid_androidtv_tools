fa3f2900325e10bc583c95fd8ed08e792065fa41
maleicacid <4982384+kazuki0824@users.noreply.github.com>
2026-08-13 15:57:49 +0900

    A15

--- get_android_qcow2.sh
+++ get_android_qcow2.sh
@@ -11,7 +11,7 @@ PRODUCT="${ANDROID_PRODUCT:-virtio_x86_64_tv_grub}"
 
 mkdir -p .repo/local_manifests/
 command cp -fv "${SCRIPTDIR}"/*.xml .repo/local_manifests/
-repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs
 repo sync -j"$(nproc)" -c --force-remove-dirty || true
 
 bash -lc "
