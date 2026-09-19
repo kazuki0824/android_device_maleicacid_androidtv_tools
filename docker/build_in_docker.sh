51b5fce2028722eb36f289293306291cba5a1d6d
maleicacid <4982384+kazuki0824@users.noreply.github.com>
2026-09-19 21:01:36 +0900

    Update build_in_docker.sh

--- docker/build_in_docker.sh
+++ docker/build_in_docker.sh
@@ -31,7 +31,8 @@ if [[ "${TUNER_AIDL_UPDATE_API:-0}" == "1" ]]; then
   UPDATE_API_CMD='m android.hardware.tv.tuner-update-api'
 fi
 
-sudo docker run --rm -i \
+sudo docker run -it \
+  --name lineage-build \
   --mount "type=bind,src=$PWD/ccache,dst=/home/builder/.ccache" \
   -v "$(pwd):/workspace" \
   -w /workspace \
