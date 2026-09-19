f71e78ad71a65a36e3ac4f39c3f6128f4a7a7dd9
maleicacid <4982384+kazuki0824@users.noreply.github.com>
2026-09-19 21:11:54 +0900

    Revert "Update build_in_docker.sh"
    This reverts commit 51b5fce2028722eb36f289293306291cba5a1d6d.

--- docker/build_in_docker.sh
+++ docker/build_in_docker.sh
@@ -31,8 +31,7 @@ if [[ "${TUNER_AIDL_UPDATE_API:-0}" == "1" ]]; then
   UPDATE_API_CMD='m android.hardware.tv.tuner-update-api'
 fi
 
-sudo docker run -it \
-  --name lineage-build \
+sudo docker run --rm -i \
   --mount "type=bind,src=$PWD/ccache,dst=/home/builder/.ccache" \
   -v "$(pwd):/workspace" \
   -w /workspace \
